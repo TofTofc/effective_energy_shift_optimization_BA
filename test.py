b6214e3104b69b1aa37c4d3e1f87c9cde75fe1f4
TofTofc <besold.christian@gmail.com>
2025-10-30 19:05:24 +0100

    Testing correct function now

--- test.py
+++ test.py
@@ -4,48 +4,34 @@ import cProfile, pstats
 
 array_length = 10000
 
+start_time_phases = np.arange(array_length)
+
 worst_case_scenario = False
 
 if worst_case_scenario:
-    power_generation = np.arange(array_length, 0, -1)
-    power_demand = np.arange(1, array_length+1)
+    energy_excess = np.arange(array_length, 0, -1)
+    energy_deficit = np.arange(1, array_length+1)
 
 else:
     seed = 12345
     rng = np.random.default_rng(seed)
 
-    power_generation = rng.integers(0, 10, array_length)
-    power_demand = rng.integers(0, 10, array_length)
+    energy_excess = rng.integers(0, 10, array_length)
+    energy_deficit = rng.integers(0, 10, array_length)
 
 delta_time_step = 1
 
 # Run the algorithm
 
 prof = cProfile.Profile()
-result = prof.runcall(efes.perform_effective_energy_shift,power_generation, power_demand, delta_time_step)
+result = prof.runcall(efes.process_phases,energy_excess, energy_deficit, start_time_phases)
+
 ps = pstats.Stats(prof).sort_stats("cumtime")
 ps.sort_stats("cumtime")
 ps.print_stats("effective_energy_shift_optimization_BA")
 
-'''
-ps.print_callees("perform_effective_energy_shift ")
-ps.print_callees("analyse_power_data")
+
 ps.print_callees("process_phases")
 ps.print_callees("move_overflow")
 ps.print_callees("add_excess_to_phase")
 ps.print_callees("remove_excess")
-'''
-
-print("input:")
-print(result.analysis_results.data_input)
-print("capacity:")
-print(result.analysis_results.capacity)
-print("energy_additional:")
-print(result.analysis_results.energy_additional)
-print("self_sufficiency:")
-print(result.analysis_results.self_sufficiency)
-print("self_consumption:")
-print(result.analysis_results.self_consumption)
-
-
-
