60ab137050cfa09b24fa533e848ed9e8d92e3455
TofTofc <besold.christian@gmail.com>
2026-01-04 00:38:35 +0100

    added option to only plot selected versions

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     #to_plot = ["test", "new_version_fusion_phaseless_2_array_structure"]
     #plot_from_json(cfg, to_plot)
 
-    #log_log_linear_regression(cfg, "new_version_fusion_phaseless_2_array_structure", min_phase_count = 1000, max_phase_count = 30000000)
+    #log_log_linear_regression(cfg, "new_version_fusion_phaseless_2_array_structure", min_phase_count = 5000)
 
     #submethod_analysis("new_version_fusion_phaseless_2_array_structure")
 
